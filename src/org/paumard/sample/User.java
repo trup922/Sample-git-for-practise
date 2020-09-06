3b6a8a22e132e26df40b51bd22c11237fef0de9f
Trup <trup922@gmail.com>
2020-09-06 09:02:40 +0530

    Added constructors

--- src/org/paumard/sample/User.java
+++ src/org/paumard/sample/User.java
@@ -3,6 +3,17 @@ package org.paumard.sample;
 public class User {
 	private String name;
 	private int age;
+	
+
+	public User() {
+		super();
+	}
+
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
